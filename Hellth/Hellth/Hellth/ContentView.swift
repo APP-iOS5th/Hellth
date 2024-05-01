770856ff645a96658f330e21828dfc3383a3c560
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-05-01 09:24:57 +0900

    Initail Commit

--- Hellth/Hellth/Hellth/ContentView.swift
+++ Hellth/Hellth/Hellth/ContentView.swift
@@ -27,7 +27,6 @@ struct ContentView: View {
                         Label("Profile", systemImage: "person")
                     }
             }
-            .accentColor(.green)
         }
     }
 }
