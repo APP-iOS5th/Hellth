b00a8876268e3e5d1034d6fc048c64b5d4f6ead3
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2024-05-02 01:31:09 +0900

    Feat: HomeModel add CalorieInTake, CalorieNutrient struct && CalorieCircleView progress

--- Hellth/Hellth/Hellth/Home/Features/HomeModel.swift
+++ Hellth/Hellth/Hellth/Home/Features/HomeModel.swift
@@ -1,12 +1,15 @@
 import Foundation
 
-struct Food: Identifiable,Codable {
-    var id = UUID()
-    let name: String
-    let calories: Int
-    let carbohydrates: Int
-    let protein: Int
-    let fat: Int
+
+struct CalorieInTake {
+    var totalCalories: Int
+    var consumedCalories: Int = 0
+    var remianingCalories: Int {
+        return totalCalories - consumedCalories
+    }
+    mutating func consumeCalories(_ calories: Int) {
+        consumedCalories += calories
+    }
 }
 
 
@@ -16,6 +19,25 @@ struct CalorieNutrient {
     let measurement: Double
 }
 
+struct Food: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let calories: Int
+    let carbohydrates: Int
+    let protein: Int
+    let fat: Int
+    
+}
+
+
+extension CalorieNutrient {
+    static let sampleNutrients: [CalorieNutrient] = [
+        CalorieNutrient(name: "Carbon", value: "50g", measurement: 2),
+        CalorieNutrient(name: "Protein", value: "50g", measurement: 2),
+        CalorieNutrient(name: "Fat", value: "50g", measurement: 2)
+    ]
+    
+}
 
 extension Food {
     static let foods: [Food] = [
@@ -26,11 +48,3 @@ extension Food {
     ]
 }
 
-extension CalorieNutrient {
-    static let sampleNutrients: [CalorieNutrient] = [
-        CalorieNutrient(name: "Carbon", value: "50g", measurement: 2),
-        CalorieNutrient(name: "Protein", value: "50g", measurement: 2),
-        CalorieNutrient(name: "Fat", value: "50g", measurement: 2)
-    ]
-    
-}
--- Hellth/Hellth/Hellth/Home/Views/CalorieCircleView.swift
+++ Hellth/Hellth/Hellth/Home/Views/CalorieCircleView.swift
@@ -1,25 +1,36 @@
 import SwiftUI
 
 struct CalorieCircleView: View {
+    var totalCalories: Int = 1000
+    var consumedCalories: Int = 800
+    var remainingCalories: Int {
+        return totalCalories - consumedCalories
+    }
     var body: some View {
+        let progress = Double(consumedCalories) / Double(totalCalories)
+        let randomColors: [Color] = [ .red, .orange, .yellow, .green, .blue, .purple, .pink, .teal, .brown, .gray, .black, .white, .mint, .indigo]
+        
         ZStack {
             Circle()
                 .stroke(Color.gray.opacity(0.2),lineWidth: 20)
+                .fill(Color.white)
+                .shadow(color: .gray, radius: 3, x: 4, y: 4)
                 .overlay(
                     VStack {
                         Spacer()
-                        Text("Goal: 1000")
+                        Text("total: \(totalCalories) cal")
                         Spacer()
-                        Text("Current Calorie: 300")
+                        Text("Current Calorie: \(consumedCalories)")
                         Spacer()
-                        Text("Remain Calorie: 700")
+                        Text("Remain Calorie: \(remainingCalories)")
                         Spacer()}
                 )
             Circle()
-                .stroke()
-                .fill(Color.white)
+                .trim(from: 0, to: CGFloat(progress))
+                .stroke(AngularGradient(gradient: Gradient(colors:randomColors.shuffled()), center: .center), lineWidth: 10)
+                .rotationEffect(.degrees(-90))
+                .animation(.easeInOut(duration: 1.0), value: progress)
                 .shadow(color: .gray, radius: 3, x: 8, y: 8)
-            
         }
         .padding()
     }
