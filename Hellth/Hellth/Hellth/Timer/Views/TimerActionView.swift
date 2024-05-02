0baea800a8aabcff8be5687f8726be04fde38ab0
Jin Park <jinpark1504@gmail.com>
2024-05-02 14:28:13 +0900

    Add: timer counting

--- Hellth/Hellth/Hellth/Timer/Views/TimerActionView.swift
+++ Hellth/Hellth/Hellth/Timer/Views/TimerActionView.swift
@@ -10,17 +10,36 @@ import SwiftUI
 struct TimerActionView: View {
     let startDateTime: Date
     let durationHour: Int
-    private let currentDate = Date.now
     
     @State private var remainingSeconds: Double = 0.0
+    @State private var durationSeconds: Int = 0
+    @State private var isFasting: Bool = false
     
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    init(startDateTime: Date, durationHour: Int) {
+        self.startDateTime = startDateTime
+        self.durationHour = durationHour
+        self.durationSeconds = durationHour * 60 * 60
+    }
     var body: some View {
-        let totalSeconds = startDateTime.timeIntervalSince(currentDate)
         VStack {
             Text("Duration Hour: \(durationHour)")
-            Text("Start: \(startDateTime), \nCurrent: \(currentDate)")
-            Text("\(startDateTime.timeIntervalSince(currentDate))")
-            Text("단식 시작까지")
+            Text("\(isFasting ? "\(durationHour)시간 단식중" : "단식 시작까지")")
+            Text("\(isFasting ? durationSeconds : Int(remainingSeconds))")
+                .onReceive(timer){ time in
+                    if !isFasting {
+                        self.remainingSeconds = startDateTime.timeIntervalSince(Date())
+                        if remainingSeconds < 0 {
+                            isFasting.toggle()
+                        }
+                    } else if self.durationSeconds > 0{
+                        self.durationSeconds -= 1
+                    }
+                    
+                }
+                .onAppear(perform: {
+                    self.remainingSeconds = startDateTime.timeIntervalSince(Date())
+                })
         }
     }
 }
